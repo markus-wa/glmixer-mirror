15b82247d9ad29db0835692e575f3771dac12a85
bruno.herbelin <bruno.herbelin@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2010-09-28 14:52:22 +0000

    Fixed the script for windows.
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@253 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

--- FindGLEW.cmake
+++ FindGLEW.cmake
@@ -7,36 +7,37 @@
 # GLEW_LIBRARY
 # 
 
-IF (WIN32)
-	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-		$ENV{PROGRAMFILES}/GLEW/include
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/include
-		DOC "The directory where GL/glew.h resides")
-	FIND_LIBRARY( GLEW_LIBRARY
-		NAMES glew GLEW glew32 glew32s 
-		PATHS
-		$ENV{PROGRAMFILES}/GLEW/lib
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
-		DOC "The GLEW library")
-ELSE (WIN32)
-	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-		/usr/include
-		/usr/local/include
-		/sw/include
-		/opt/local/include
-		DOC "The directory where GL/glew.h resides")
-	FIND_LIBRARY( GLEW_LIBRARY
-		NAMES GLEW glew
-		PATHS
-		/usr/lib64
-		/usr/lib
-		/usr/local/lib64
-		/usr/local/lib
-		/sw/lib
-		/opt/local/lib
-		DOC "The GLEW library")
-ENDIF (WIN32)
+
+FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+  HINTS
+  PATH_SUFFIXES include
+  PATHS
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local/include
+  /usr/include
+  /sw/include
+  /opt/local/include
+  /opt/csw/include 
+  /opt/include
+  /mingw
+  DOC "The directory where GL/glew.h resides"
+)
+ 
+FIND_LIBRARY( GLEW_LIBRARY
+	NAMES GLEW glew glew32
+  HINTS
+  PATH_SUFFIXES lib64 lib
+  PATHS
+  /usr/local
+  /usr
+  /sw
+  /opt/local
+  /opt/csw
+  /opt
+  /mingw
+	DOC "The GLEW library"
+)
 
 IF (GLEW_INCLUDE_PATH)
 	SET( GLEW_FOUND "YES" CACHE STRING "Set to YES if GLEW is found, not set otherwise")
