0708f72a9675616e1c021bb49960a4fd231d2fb1
bruno.herbelin@gmail.com <bruno.herbelin@gmail.com@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2013-08-04 14:37:16 +0000

    Creation of a FreeFrameGL delay plugin (not effective yet)
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@690 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

---
+++ FreeFramePlugins/FreeFrameDelayPlugin/FreeFrameDelay.cpp
@@ -0,0 +1,140 @@
+#include <FFGL.h>
+#include <FFGLLib.h>
+
+#include <cmath>
+
+#include "FreeFrameDelay.h"
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Plugin information
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static CFFGLPluginInfo PluginInfo ( 
+    FreeFrameDelay::CreateInstance,	// Create method
+    "GLDLY",								// Plugin unique ID
+	"FFGLDelay",			// Plugin name											
+	1,						   			// API major version number 													
+	000,								  // API minor version number	
+	1,										// Plugin major version number
+	000,									// Plugin minor version number
+	FF_EFFECT,						// Plugin type
+    "Delay the display of the provided input",	 // Plugin description
+    "by Bruno Herbelin"  // About
+);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Constructor and destructor
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FreeFrameDelay::FreeFrameDelay()
+: CFreeFrameGLPlugin()
+{
+    // Input properties
+    SetMinInputs(1);
+    SetMaxInputs(1);
+    SetTimeSupported(true);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Methods
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef FF_FAIL
+    // FFGL 1.5
+    DWORD   FreeFrameDelay::InitGL(const FFGLViewportStruct *vp)
+#else
+    // FFGL 1.6
+    FFResult FreeFrameDelay::InitGL(const FFGLViewportStruct *vp)
+#endif
+{
+
+    return FF_SUCCESS;
+}
+
+
+#ifdef FF_FAIL
+    // FFGL 1.5
+    DWORD   FreeFrameDelay::DeInitGL()
+#else
+    // FFGL 1.6
+    FFResult FreeFrameDelay::DeInitGL()
+#endif
+{
+
+  return FF_SUCCESS;
+}
+
+#ifdef FF_FAIL
+    // FFGL 1.5
+    DWORD   FreeFrameDelay::SetTime(double time)
+#else
+    // FFGL 1.6
+    FFResult FreeFrameDelay::SetTime(double time)
+#endif
+{
+  m_curTime = time;
+  return FF_SUCCESS;
+}
+
+#ifdef FF_FAIL
+    // FFGL 1.5
+    DWORD	FreeFrameDelay::ProcessOpenGL(ProcessOpenGLStruct* pGL)
+#else
+    // FFGL 1.6
+    FFResult FreeFrameDelay::ProcessOpenGL(ProcessOpenGLStruct *pGL)
+#endif
+{
+  if (pGL->numInputTextures<1)
+    return FF_FAIL;
+
+  if (pGL->inputTextures[0]==NULL)
+    return FF_FAIL;
+  
+  FFGLTextureStruct &Texture = *(pGL->inputTextures[0]);
+
+  //bind the texture handle to its target
+  glBindTexture(GL_TEXTURE_2D, Texture.Handle);
+
+  //enable texturemapping
+  glEnable(GL_TEXTURE_2D);
+
+  //get the max s,t that correspond to the 
+  //width,height of the used portion of the allocated texture space
+  FFGLTexCoords maxCoords = GetMaxGLTexCoords(Texture);
+
+  //modulate texture colors with white (just show
+  //the texture colors as they are)
+  glColor4f(1.f, 1.f, 1.f, 1.f);
+  //(default texturemapping behavior of OpenGL is to
+  //multiply texture colors by the current gl color)
+  
+  glBegin(GL_QUADS);
+
+  //lower left
+  glTexCoord2d(0.0, 0.0);
+  glVertex2f(-1,-1);
+
+  //upper left
+  glTexCoord2d(0.0, maxCoords.t);
+  glVertex2f(-1,1);
+
+  //upper right
+  glTexCoord2d(maxCoords.s, maxCoords.t);
+  glVertex2f(1,1);
+
+  //lower right
+  glTexCoord2d(maxCoords.s, 0.0);
+  glVertex2f(1,-1);
+  glEnd();
+
+  //unbind the texture
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  //disable texturemapping
+  glDisable(GL_TEXTURE_2D);
+
+  return FF_SUCCESS;
+}
