bbdb7b14d9da61218bce1f762531ba4361442fca
bruno.herbelin@gmail.com <bruno.herbelin@gmail.com@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2013-10-18 08:37:38 +0000

    Creation of the Freeframe plugin 'Wave'
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@726 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

---
+++ FreeFramePlugins/FreeFrameWavePlugin/FreeFrameWave.cpp
@@ -0,0 +1,156 @@
+#include <FFGL.h>
+#include <FFGLLib.h>
+#include "FreeFrameWave.h"
+
+#include <cmath>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Plugin information
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static CFFGLPluginInfo PluginInfo ( 
+    FreeFrameWave::CreateInstance,	// Create method
+    "GLwAVE",           // Plugin unique ID
+    "FreeFrameWave",    // Plugin name
+    1,                  // API major version number
+    500,                // API minor version number
+    1,                  // Plugin major version number
+    000,                // Plugin minor version number
+    FF_EFFECT,          // Plugin type
+    "Wavy plugin",      // Plugin description
+    "by Bruno Herbelin" // About
+);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Constructor and destructor
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FreeFrameWave::FreeFrameWave()
+: CFreeFrameGLPlugin()
+{
+    // Input properties
+    SetMinInputs(1);
+    SetMaxInputs(1);
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Methods
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD   FreeFrameWave::InitGL(const FFGLViewportStruct *vp)
+#else
+// FFGL 1.6
+FFResult FreeFrameWave::InitGL(const FFGLViewportStruct *vp)
+#endif
+{
+
+    width = vp->width;
+    height = vp->height;
+
+    return FF_SUCCESS;
+}
+
+
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD   FreeFrameWave::DeInitGL()
+#else
+// FFGL 1.6
+FFResult FreeFrameWave::DeInitGL()
+#endif
+{
+
+  return FF_SUCCESS;
+}
+
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD   FreeFrameWave::SetTime(double time)
+#else
+// FFGL 1.6
+FFResult FreeFrameWave::SetTime(double time)
+#endif
+{
+  m_curTime = time;
+  return FF_SUCCESS;
+}
+
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD	FreeFrameWave::ProcessOpenGL(ProcessOpenGLStruct* pGL)
+#else
+// FFGL 1.6
+FFResult FreeFrameWave::ProcessOpenGL(ProcessOpenGLStruct *pGL)
+#endif
+{
+  if (pGL->numInputTextures<1)
+    return FF_FAIL;
+
+  if (pGL->inputTextures[0]==NULL)
+    return FF_FAIL;
+  
+  FFGLTextureStruct &Texture = *(pGL->inputTextures[0]);
+
+  //bind the texture handle to its target
+  glBindTexture(GL_TEXTURE_2D, Texture.Handle);
+
+  //enable texturemapping
+  glEnable(GL_TEXTURE_2D);
+
+  //get the max s,t that correspond to the 
+  //width,height of the used portion of the allocated texture space
+ // FFGLTexCoords maxCoords = GetMaxGLTexCoords(Texture);
+
+  //modulate texture colors with white (just show
+  //the texture colors as they are)
+  glColor4f(1.f, 1.f, 1.f, 1.f);
+  
+  unsigned long tick = (unsigned long) (m_curTime * 1000);
+  int i, j;
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  int wmargin = width / 32;
+  int hmargin = height / 32;
+  glOrtho(wmargin, width-wmargin, hmargin, height-hmargin, -1, 1);
+
+  glMatrixMode(GL_MODELVIEW);
+
+  // Generate a 16x16 grid, and perturb the UV coordinates
+  glBegin(GL_TRIANGLE_STRIP);
+  for (j = 0; j < 16; j++)
+  {
+      for (i = 0; i < 17; i++)
+      {
+          float u, v;
+          u = i * (1 / 16.0f);
+          v = j * (1 / 16.0f);
+          u += (float)((sin((tick + i*100) * 0.012387) * 0.04) * sin(tick * 0.000514382));
+          v += (float)((cos((tick + j*100) * 0.012387) * 0.04) * sin(tick * 0.000714282));
+          glTexCoord2f(u, v);
+          glVertex2f(i * (width / 16.0f), j * (height / 16.0f));
+
+          u = i * (1 / 16.0f);
+          v = (j + 1) * (1 / 16.0f);
+          u += (float)((sin((tick +       i*100) * 0.012387) * 0.04) * sin(tick * 0.000514382));
+          v += (float)((cos((tick + (j + 1)*100) * 0.012387) * 0.04) * sin(tick * 0.000714282));
+          glTexCoord2f(u, v);
+          glVertex2f(i * (width / 16.0f), (j + 1) * (height / 16.0f));
+      }
+      // create some degenerate surfaces to "turn" the strip after each horizontal span
+      glVertex2f(width, (j + 1) * (height / 16.0f));
+      glVertex2f(0, (j + 2) * (height / 16.0f));
+  }
+  glEnd();
+
+  //unbind the texture
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  //disable texturemapping
+  glDisable(GL_TEXTURE_2D);
+
+  return FF_SUCCESS;
+}
---
+++ FreeFramePlugins/FreeFrameWavePlugin/FreeFrameWave.h
@@ -0,0 +1,54 @@
+#ifndef FFGLFFTEST_H
+#define FFGLFFTEST_H
+
+#include <FFGLPluginSDK.h>
+
+class FreeFrameWave : public CFreeFrameGLPlugin
+{
+public:
+    FreeFrameWave();
+    virtual ~FreeFrameWave() {}
+
+	///////////////////////////////////////////////////
+	// FreeFrame plugin methods
+	///////////////////////////////////////////////////
+#ifdef FF_FAIL
+    // FFGL 1.5
+    DWORD	ProcessOpenGL(ProcessOpenGLStruct* pGL);
+    DWORD   SetTime(double time);
+    DWORD   InitGL(const FFGLViewportStruct *vp);
+    DWORD   DeInitGL();
+#else
+    // FFGL 1.6
+    FFResult    ProcessOpenGL(ProcessOpenGLStruct* pGL);
+    FFResult    SetTime(double time);
+    FFResult    InitGL(const FFGLViewportStruct *vp);
+    FFResult    DeInitGL();
+#endif
+	///////////////////////////////////////////////////
+	// Factory method
+	///////////////////////////////////////////////////
+
+#ifdef FF_FAIL
+    // FFGL 1.5
+    static DWORD __stdcall CreateInstance(CFreeFrameGLPlugin **ppOutInstance)
+#else
+    // FFGL 1.6
+	static FFResult __stdcall CreateInstance(CFreeFrameGLPlugin **ppOutInstance)
+#endif
+    {
+        *ppOutInstance = new FreeFrameWave();
+        if (*ppOutInstance != NULL)
+            return FF_SUCCESS;
+        return FF_FAIL;
+    }
+
+protected:
+    // Time
+    double m_curTime;
+    int width;
+    int height;
+};
+
+
+#endif
