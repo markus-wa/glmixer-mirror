ffb388c402adb8f3dc8369ca8bad94bbd6191ffe
bruno.herbelin@gmail.com <bruno.herbelin@gmail.com@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2013-10-18 15:58:14 +0000

    Creation of FreeFrameBlur plugin
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@732 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

---
+++ FreeFramePlugins/FreeFrameBlurPlugin/FreeFrameBlur.cpp
@@ -0,0 +1,253 @@
+
+#include <GL/glew.h>
+
+#include <FFGL.h>
+#include <FFGLLib.h>
+#include <stdio.h>
+
+#include "FreeFrameBlur.h"
+
+#define FFPARAM_BLUR (0)
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Plugin information
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static CFFGLPluginInfo PluginInfo ( 
+    FreeFrameBlur::CreateInstance,	// Create method
+    "GLBLR",								// Plugin unique ID
+    "FFGLBlur",			// Plugin name
+	1,						   			// API major version number 													
+    500,								  // API minor version number
+	1,										// Plugin major version number
+	000,									// Plugin minor version number
+	FF_EFFECT,						// Plugin type
+    "Blurs the display of the provided input",	 // Plugin description
+    "by Bruno Herbelin"  // About
+);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Constructor and destructor
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FreeFrameBlur::FreeFrameBlur()
+: CFreeFrameGLPlugin()
+{
+    // Input properties
+    SetMinInputs(1);
+    SetMaxInputs(1);
+    SetTimeSupported(true);
+
+    // Parameters
+    SetParamInfo(FFPARAM_BLUR, "Blur", FF_TYPE_STANDARD, "0.5");
+    blur = 0.5;
+    param_changed = true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Methods
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD   FreeFrameBlur::InitGL(const FFGLViewportStruct *vp)
+#else
+// FFGL 1.6
+FFResult FreeFrameBlur::InitGL(const FFGLViewportStruct *vp)
+#endif
+{
+    viewport.x = 0;
+    viewport.y = 0;
+    viewport.width = vp->width;
+    viewport.height = vp->height;
+
+
+    //init gl extensions
+    glExtensions.Initialize();
+    if (glExtensions.EXT_framebuffer_object==0)
+      return FF_FAIL;
+
+    return FF_SUCCESS;
+}
+
+
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD   FreeFrameBlur::DeInitGL()
+#else
+// FFGL 1.6
+FFResult FreeFrameBlur::DeInitGL()
+#endif
+{
+
+    fbo.FreeResources(glExtensions);
+
+  return FF_SUCCESS;
+}
+
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD   FreeFrameBlur::SetTime(double time)
+#else
+// FFGL 1.6
+FFResult FreeFrameBlur::SetTime(double time)
+#endif
+{
+  m_curTime = time;
+  return FF_SUCCESS;
+}
+
+void drawQuad( FFGLViewportStruct vp, FFGLTextureStruct texture )
+{
+    FFGLTexCoords maxCoords = GetMaxGLTexCoords(texture);
+
+    glBindTexture(GL_TEXTURE_2D, texture.Handle);
+
+    //modulate texture colors with white (just show
+    //the texture colors as they are)
+    glColor4f(1.f, 1.f, 1.f, 1.f);
+
+    // setup display
+    glViewport(vp.x,vp.y,vp.width,vp.height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glBegin(GL_QUADS);
+    //lower left
+    glTexCoord2d(0.0, 0.0);
+    glVertex2f(-1,-1);
+
+    //upper left
+    glTexCoord2d(0.0, maxCoords.t);
+    glVertex2f(-1,1);
+
+    //upper right
+    glTexCoord2d(maxCoords.s, maxCoords.t);
+    glVertex2f(1,1);
+
+    //lower right
+    glTexCoord2d(maxCoords.s, 0.0);
+    glVertex2f(1,-1);
+    glEnd();
+
+}
+
+
+#ifdef FF_FAIL
+// FFGL 1.5
+DWORD	FreeFrameBlur::ProcessOpenGL(ProcessOpenGLStruct* pGL)
+#else
+// FFGL 1.6
+FFResult FreeFrameBlur::ProcessOpenGL(ProcessOpenGLStruct *pGL)
+#endif
+{
+  // get the input texture
+  if (pGL->numInputTextures<1)
+    return FF_FAIL;
+
+  if (pGL->inputTextures[0]==NULL)
+    return FF_FAIL;
+  
+  FFGLTextureStruct &Texture = *(pGL->inputTextures[0]);
+
+  //enable texturemapping
+  glEnable(GL_TEXTURE_2D);
+
+  // new value of the blur parameter
+  if(param_changed) {
+
+      glExtensions.glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+
+      fboViewport.x = 0;
+      fboViewport.y = 0;
+      fboViewport.width = (int)((double)viewport.width * (1.0 - blur) );
+      fboViewport.height = (int)((double)viewport.height * (1.0 - blur) );
+
+      // sanity check for size
+      fboViewport.width = fboViewport.width < 1 ? 1 : fboViewport.width;
+      fboViewport.height = fboViewport.height < 1 ? 1 : fboViewport.height;
+
+      fbo.FreeResources(glExtensions);
+      if (! fbo.Create( fboViewport.width, fboViewport.height, glExtensions) )
+          return FF_FAIL;
+
+      param_changed = false;
+  }
+
+  // activate the fbo as our render target
+  if (!fbo.BindAsRenderTarget(glExtensions))
+    return FF_FAIL;
+
+  // no depth test
+  glDisable(GL_DEPTH_TEST);
+
+  //render the original texture on a quad
+  drawQuad( fboViewport, Texture);
+
+  // (re)activate the HOST fbo as render target
+  glExtensions.glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, pGL->HostFBO);
+
+  // render the fbo texture texture on a quad
+  drawQuad( viewport, fbo.GetTextureInfo() );
+
+  //unbind the texture
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  //disable texturemapping
+  glDisable(GL_TEXTURE_2D);
+
+  return FF_SUCCESS;
+}
+
+
+#ifdef FF_FAIL
+    // FFGL 1.5
+    DWORD FreeFrameBlur::SetParameter(const SetParameterStruct* pParam)
+    {
+        if (pParam != NULL && pParam->ParameterNumber == FFPARAM_BLUR) {
+            blur = *((float *)(unsigned)&(pParam->NewParameterValue));
+            param_changed = true;
+            return FF_SUCCESS;
+        }
+
+        return FF_FAIL;
+    }
+
+    DWORD FreeFrameBlur::GetParameter(DWORD index)
+    {
+        DWORD dwRet = FF_FAIL;
+
+        if (index == FFPARAM_BLUR)
+            *((float *)(unsigned)&dwRet) = blur;
+
+        return dwRet;
+    }
+
+#else
+    // FFGL 1.6
+    FFResult FreeFrameBlur::SetFloatParameter(unsigned int index, float value)
+    {
+        if (index == FFPARAM_BLUR) {
+            blur = value;
+            param_changed = true;
+            return FF_SUCCESS;
+        }
+
+        return FF_FAIL;
+    }
+
+    float FreeFrameBlur::GetFloatParameter(unsigned int index)
+    {
+        if (index == FFPARAM_BLUR)
+            return blur;
+
+        return 0.0;
+    }
+#endif
+
+
+
+
---
+++ FreeFramePlugins/FreeFrameBlurPlugin/FreeFrameBlur.h
@@ -0,0 +1,64 @@
+#ifndef FFGLMirror_H
+#define FFGLMirror_H
+
+#include <FFGLPluginSDK.h>
+#include <FFGLFBO.h>
+
+class FreeFrameBlur : public CFreeFrameGLPlugin
+{
+public:
+    FreeFrameBlur();
+    virtual ~FreeFrameBlur() {}
+
+	///////////////////////////////////////////////////
+	// FreeFrame plugin methods
+	///////////////////////////////////////////////////
+#ifdef FF_FAIL
+    // FFGL 1.5
+    DWORD	ProcessOpenGL(ProcessOpenGLStruct* pGL);
+    DWORD   SetTime(double time);
+    DWORD   InitGL(const FFGLViewportStruct *vp);
+    DWORD   DeInitGL();
+    DWORD	SetParameter(const SetParameterStruct* pParam);
+    DWORD	GetParameter(DWORD dwIndex);
+#else
+    // FFGL 1.6
+    FFResult    ProcessOpenGL(ProcessOpenGLStruct* pGL);
+    FFResult    SetTime(double time);
+    FFResult    InitGL(const FFGLViewportStruct *vp);
+    FFResult    DeInitGL();
+    FFResult	SetFloatParameter(unsigned int index, float value);
+    float		GetFloatParameter(unsigned int index);
+#endif
+
+	///////////////////////////////////////////////////
+	// Factory method
+	///////////////////////////////////////////////////
+
+#ifdef FF_FAIL
+    // FFGL 1.5
+    static DWORD __stdcall CreateInstance(CFreeFrameGLPlugin **ppOutInstance)
+#else
+    // FFGL 1.6
+	static FFResult __stdcall CreateInstance(CFreeFrameGLPlugin **ppOutInstance)
+#endif
+    {
+        *ppOutInstance = new FreeFrameBlur();
+        if (*ppOutInstance != NULL)
+            return FF_SUCCESS;
+        return FF_FAIL;
+    }
+
+protected:
+    // Time
+    double m_curTime;
+    double blur;
+    bool param_changed;
+    FFGLViewportStruct viewport;
+    FFGLViewportStruct fboViewport;
+    FFGLExtensions glExtensions;
+    FFGLFBO fbo;
+};
+
+
+#endif
