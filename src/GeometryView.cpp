0638f15e54bb51426eb0033e8e9e6c812fa19e14
bruno.herbelin@gmail.com <bruno.herbelin@gmail.com@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2014-10-17 17:03:43 +0000

    Bug fix on last bug fix :( ... (r867 introduced a bug in selection management)
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@923 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

--- src/GeometryView.cpp
+++ src/GeometryView.cpp
@@ -1427,16 +1427,18 @@ Source *GeometryView::getCurrentSource()
 {
     // first is the current source of the rendering manager valid ?
     if (RenderingManager::getInstance()->isValid(RenderingManager::getInstance()->getCurrentSource()))
-    {
-        // if yes, set it as current (it may has been selected in another view
+        // if yes, set it as current (it may has been selected in another view)
         currentSource = *RenderingManager::getInstance()->getCurrentSource();
 
-        // if current source is a valid source
-        if (currentSource && currentSource != SelectionManager::getInstance()->selectionSource())
-            // return it only if modifiable
+    // if current source is a valid source
+    if (currentSource) {
+
+        // if the current source is not the selection source
+        if (currentSource != SelectionManager::getInstance()->selectionSource())
+            // return the source only if modifiable
             return currentSource->isModifiable() ? currentSource : 0;
         else
-            // or just return the value
+            // or just return the selection source
             return currentSource;
     }
 
