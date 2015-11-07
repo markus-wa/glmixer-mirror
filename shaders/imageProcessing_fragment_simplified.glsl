d94c25509148cfcbab18f27f6cd28bc568a434c8
brunoherbelin <brunoherbelin@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2015-11-07 12:31:04 +0000

    GLSL 1.10 implementation of stippling (no warning).
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@1047 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

--- shaders/imageProcessing_fragment_simplified.glsl
+++ shaders/imageProcessing_fragment_simplified.glsl
@@ -136,7 +136,7 @@ void main(void)
     alpha -= mix( 0.0, step( length( normalize(chromakey.xyz) - normalize(transformedRGB) ), chromadelta ), float(chromakey.w > 0.0) );
 
     // stippling
-    alpha += float ( int(floor(gl_FragCoord.x * stippling) + floor(gl_FragCoord.y * stippling)) % 2 );
+    alpha += mod( floor(gl_FragCoord.x * stippling) + floor(gl_FragCoord.y * stippling), 2.0);
 
     // apply base color and alpha for final fragment color
     gl_FragColor =  vec4(transformedRGB * baseColor.rgb, clamp(alpha, 0.0, 1.0)  );
