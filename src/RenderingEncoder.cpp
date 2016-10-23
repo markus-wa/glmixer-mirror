d944395250c68631df8cb28c8d10f6339094902d
brunoherbelin <brunoherbelin@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2016-10-23 23:24:25 +0000

    Fixbug : saving recording after selection of filename.
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@1222 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

--- src/RenderingEncoder.cpp
+++ src/RenderingEncoder.cpp
@@ -157,39 +157,39 @@ bool EncodingThread::pictq_full() {
 
 void EncodingThread::run() {
 
-	// prepare
+    // prepare
     _quit = false;
     int pictq_usage = 0, picq_size_usage = 0;
 
-	// loop until break
-	while (true) {
+    // loop until break
+    while (true) {
 
         if (pictq_size_count < 1) {
-			// no picture ?
-			// if it is because we shall quit, then terminate thread
+            // no picture ?
+            // if it is because we shall quit, then terminate thread
             if (_quit)
                 break;
 
         } else {
 
-			// record the picture to encode by calling the function specified in the recorder
+            // record the picture to encode by calling the function specified in the recorder
             (*(rec)->pt2RecordingFunction)(rec, pictq[pictq_rindex], recordingTimeStamp[pictq_rindex]);
 
-			/* update queue for next picture at the read index */
+            /* update queue for next picture at the read index */
             if (++pictq_rindex == pictq_max_count)
-				pictq_rindex = 0;
+                pictq_rindex = 0;
 
-			pictq_mutex->lock();
+            pictq_mutex->lock();
             // remember usage
             pictq_usage = MAXI(pictq_usage, pictq_rindex + 1);
             picq_size_usage = MAXI(picq_size_usage, pictq_size_count + 1);
-			// decrease the number of frames in the queue
+            // decrease the number of frames in the queue
             pictq_size_count--;
-			// tell main process that it can go on (in case it was waiting on a full queue)
-			pictq_cond->wakeAll();
-			pictq_mutex->unlock();
+            // tell main process that it can go on (in case it was waiting on a full queue)
+            pictq_cond->wakeAll();
+            pictq_mutex->unlock();
 
-		}
+        }
 
         // maintain a slow use of ressources for encoding during the recording
         if (!_quit)
@@ -207,9 +207,9 @@ void EncodingThread::run() {
 
 RenderingEncoder::RenderingEncoder(QObject * parent): QObject(parent), started(false), paused(false), elapseTimer(0), skipframecount(0), update(40), displayupdate(33), bufferSize(DEFAULT_RECORDING_BUFFER_SIZE) {
 
-	// set default format
+    // set default format
     temporaryFileName = "__temp__";
-	setEncodingFormat(FORMAT_AVI_FFVHUFF);
+    setEncodingFormat(FORMAT_AVI_FFVHUFF);
     // init file saving
     setAutomaticSavingMode(false);
 
@@ -231,11 +231,11 @@ RenderingEncoder::~RenderingEncoder() {
 
 void RenderingEncoder::setEncodingFormat(encodingformat f){
 
-	if (!started) {
-		format = f;
-	} else {
+    if (!started) {
+        format = f;
+    } else {
         qCritical() << tr ("Cannot change video recording format; Recorder is busy.");
-	}
+    }
 
 }
 
@@ -251,8 +251,8 @@ void RenderingEncoder::setActive(bool on)
         connect(encoder, SIGNAL(encodingFinished()), this, SLOT(close()));
         connect(encoder, SIGNAL(finished()), encoder, SLOT(deleteLater()));
 
-		if (!start())
-            qCritical() << tr("Error starting video recording; ") << errormessage;          
+        if (!start())
+            qCritical() << tr("Error starting video recording; ") << errormessage;
 
     }
     else {
@@ -267,35 +267,35 @@ void RenderingEncoder::setActive(bool on)
         }
         // restore rendering fps
         glRenderWidget::setUpdatePeriod( displayupdate );
-	}
+    }
 }
 
 
 void RenderingEncoder::setPaused(bool on)
 {
-	static int elapsed = 0;
+    static int elapsed = 0;
 
-	// no pause if not active
-	if (!started)
-		return;
+    // no pause if not active
+    if (!started)
+        return;
 
-	// set pause
-	paused = on;
+    // set pause
+    paused = on;
 
-	if (paused) {
-		// remember timer
-		elapsed = timer.elapsed();
-		killTimer(elapseTimer);
+    if (paused) {
+        // remember timer
+        elapsed = timer.elapsed();
+        killTimer(elapseTimer);
         emit status(tr("Recording paused after %1 s").arg((double)elapsed/1000.0), 3000);
         qDebug() << "RenderingEncoder" << QChar(124).toLatin1() << tr("Recording paused after %1 s.").arg((double)elapsed/1000.0);
     }
     else {
-		// restart a timer
-		timer = timer.addMSecs(timer.elapsed() - elapsed);
-		elapseTimer = startTimer(1000);
+        // restart a timer
+        timer = timer.addMSecs(timer.elapsed() - elapsed);
+        elapseTimer = startTimer(1000);
         emit status(tr("Recording resumed at %1 s").arg((double)timer.elapsed()/1000.0), 1000);
         qDebug() << "RenderingEncoder" << QChar(124).toLatin1() << tr("Recording resumed at %1 s.").arg((double)elapsed/1000.0);
-	}
+    }
 
 }
 
@@ -304,62 +304,62 @@ void RenderingEncoder::setPaused(bool on)
 // - Create the temporary file
 bool RenderingEncoder::start(){
 
-	if (started) {
-		QByteArray(errormessage, 256) = "Already recording.";
-		return false;
-	}
+    if (started) {
+        QByteArray(errormessage, 256) = "Already recording.";
+        return false;
+    }
 
-	// if the temporary file already exists, delete it.
-	if (temporaryFolder.exists(temporaryFileName)){
-		temporaryFolder.remove(temporaryFileName);
-	}
+    // if the temporary file already exists, delete it.
+    if (temporaryFolder.exists(temporaryFileName)){
+        temporaryFolder.remove(temporaryFileName);
+    }
 
-	// compute desired update frequency
-	int freq = (int) ( 1000.0 / double(update) );
+    // compute desired update frequency
+    int freq = (int) ( 1000.0 / double(update) );
 
     // adjust update to match the fps
     update = (int) ( 1000.0 / double(freq));
 
     // read current frame rate
-	int fps = RenderingManager::getRenderingWidget()->getFramerate();
+    int fps = RenderingManager::getRenderingWidget()->getFramerate();
 
-	// show warning if frame rate is already too low
-	if ( fps <  freq ) {
-		 QMessageBox msgBox;
-		 msgBox.setIcon(QMessageBox::Question);
+    // show warning if frame rate is already too low
+    if ( fps <  freq ) {
+         QMessageBox msgBox;
+         msgBox.setIcon(QMessageBox::Question);
          msgBox.setText(tr("Rendering frequency is lower than the recording %1 fps.").arg(freq));
-		 msgBox.setInformativeText(tr("Do you still want to record at %1 fps ?").arg(fps));
-		 msgBox.setDetailedText( tr("The rendering is currently at %1 fps on average, but your recording preferences are set to %2 fps.\n\n"
-				 "You can either agree to record at this lower frame rate, or retry later after some optimizations:\n"
-				 "- select a lower quality in your rendering preferences\n"
-				 "- lower the resolution of some sources\n"
-				 "- remove some sources\n").arg(fps).arg(freq) );
+         msgBox.setInformativeText(tr("Do you still want to record at %1 fps ?").arg(fps));
+         msgBox.setDetailedText( tr("The rendering is currently at %1 fps on average, but your recording preferences are set to %2 fps.\n\n"
+                 "You can either agree to record at this lower frame rate, or retry later after some optimizations:\n"
+                 "- select a lower quality in your rendering preferences\n"
+                 "- lower the resolution of some sources\n"
+                 "- remove some sources\n").arg(fps).arg(freq) );
 
-		 QPushButton *abortButton = msgBox.addButton(QMessageBox::Discard);
-		 msgBox.addButton(tr("Record at lower frequency"), QMessageBox::AcceptRole);
-		 msgBox.exec();
-		 if (msgBox.clickedButton() == abortButton) {
+         QPushButton *abortButton = msgBox.addButton(QMessageBox::Discard);
+         msgBox.addButton(tr("Record at lower frequency"), QMessageBox::AcceptRole);
+         msgBox.exec();
+         if (msgBox.clickedButton() == abortButton) {
              QByteArray(errormessage, 256) = "Recording aborted.";
-		     return false;
-		 }
-		 // Continue anyway : set the recoding frequency to be at the fps of the rendering
-		 freq = fps;
-	}
+             return false;
+         }
+         // Continue anyway : set the recoding frequency to be at the fps of the rendering
+         freq = fps;
+    }
 
     // remember current update display period
-	displayupdate = glRenderWidget::updatePeriod();
+    displayupdate = glRenderWidget::updatePeriod();
 
     // setup new display update period to match recording update
-	glRenderWidget::setUpdatePeriod( update );
+    glRenderWidget::setUpdatePeriod( update );
 
-	// initialization of ffmpeg recorder
-	recorder = video_rec_init(qPrintable(temporaryFolder.absoluteFilePath(temporaryFileName)), format, framesSize.width(), framesSize.height(), freq, errormessage);
+    // initialization of ffmpeg recorder
+    recorder = video_rec_init(qPrintable(temporaryFolder.absoluteFilePath(temporaryFileName)), format, framesSize.width(), framesSize.height(), freq, errormessage);
     if (recorder == NULL) {
         QByteArray(errormessage, 256) = "Failed to initialize recorder.";
         return false;
     }
 
-	// init
+    // init
     skipframecount = 0;
 
     // compute buffer count from size
@@ -377,13 +377,13 @@ bool RenderingEncoder::start(){
     emit activated(started);
 
     // start the timers and the encoding thread
-	timer.start();
-	elapseTimer = startTimer(1000); // emit the duration of recording every second
+    timer.start();
+    elapseTimer = startTimer(1000); // emit the duration of recording every second
     encoder->start();
 
     qDebug() << temporaryFolder.absoluteFilePath(temporaryFileName) << QChar(124).toLatin1()  << tr("Recording started (%1 at %2 fps, buffer of %3 frames in %4 ).").arg(recorder->suffix).arg(freq).arg(bufCount).arg(getByteSizeString(bufferSize));
 
-	return true;
+    return true;
 }
 
 void RenderingEncoder::timerEvent(QTimerEvent *event)
@@ -393,10 +393,10 @@ void RenderingEncoder::timerEvent(QTimerEvent *event)
 
 int RenderingEncoder::getRecodingTime() {
 
-	if (started)
-		return timer.elapsed();
-	else
-		return 0;
+    if (started)
+        return timer.elapsed();
+    else
+        return 0;
 }
 
 // Add a frame to the stream
@@ -405,8 +405,8 @@ int RenderingEncoder::getRecodingTime() {
 // it *should* be called at the desired frame rate
 void RenderingEncoder::addFrame(unsigned char *data){
 
-	if (!started || paused || recorder == NULL)
-		return;
+    if (!started || paused || recorder == NULL)
+        return;
 
     // if the recorder cannot follow
     if ( encoder->pictq_full() )
@@ -435,19 +435,19 @@ void RenderingEncoder::addFrame(unsigned char *data){
 // Close the encoding process
 void RenderingEncoder::close(){
 
-	// stop recorder
+    // stop recorder
     int framecount = video_rec_stop(recorder);
 
     // done
     float duration = (float) timer.elapsed() / 1000.0;
-	emit selectAspectRatio(ASPECT_RATIO_FREE);
+    emit selectAspectRatio(ASPECT_RATIO_FREE);
     emit status(tr("Recorded %1 s").arg(duration), 3000);
     killTimer(elapseTimer);
 
     qDebug() << "RenderingEncoder" << QChar(124).toLatin1() << tr("Recording finished (%1 frames in %2 s).").arg(framecount).arg(duration);
 
     // inform we are off
-	started = false;
+    started = false;
     emit activated(false);
 
     // free encoder and stop all
@@ -462,31 +462,31 @@ void RenderingEncoder::close(){
 
     emit processing(false);
 
-	// show warning if too many frames were bad
+    // show warning if too many frames were bad
     bool savefile = true;
     float percent = float(skipframecount) / float(framecount + skipframecount);
     if ( percent > 0.05f  ) {
 
-		 QMessageBox msgBox;
-		 msgBox.setIcon(QMessageBox::Warning);
+         QMessageBox msgBox;
+         msgBox.setIcon(QMessageBox::Warning);
          msgBox.setText(tr("A movie has been recorded, but %1 % of the frames were skipped.").arg( 100.f * percent ));
          msgBox.setInformativeText(tr("Do you still want to save it ?"));
          msgBox.setDetailedText( tr("Only %1 of %2 frames were recorded. "
                  "Playback of the movie might be jerky.\n"
-				 "To avoid this, change the preferences to:\n"
+                 "To avoid this, change the preferences to:\n"
                  " - a lower rendering resolution\n"
                  " - a lower recording frame rate\n"
                  " - a faster recording codec (try MP4)").arg(framecount).arg(framecount + skipframecount));
 
-		 QPushButton *abortButton = msgBox.addButton(QMessageBox::Discard);
-		 msgBox.addButton(tr("Save anyway"), QMessageBox::AcceptRole);
+         QPushButton *abortButton = msgBox.addButton(QMessageBox::Discard);
+         msgBox.addButton(tr("Save anyway"), QMessageBox::AcceptRole);
 
          // display dialog warning
-		 msgBox.exec();
+         msgBox.exec();
          if (msgBox.clickedButton() == abortButton) {
              savefile = false;
          }
-	}
+    }
 
     // save file
     if (savefile) {
@@ -504,23 +504,23 @@ void RenderingEncoder::close(){
 
 void RenderingEncoder::setAutomaticSavingMode(bool on) {
 
-	automaticSaving = on;
+    automaticSaving = on;
 
-	// ensure the temporary file is in the same folder as the destination file
-	// to avoid copy (rename) of file accross drives
+    // ensure the temporary file is in the same folder as the destination file
+    // to avoid copy (rename) of file accross drives
     if (automaticSaving)
         temporaryFolder = savingFolder;
     else
-		temporaryFolder = QDir::temp();
+        temporaryFolder = QDir::temp();
 
 }
 
 
 void RenderingEncoder::setAutomaticSavingFolder(QDir d) {
 
-	savingFolder = d;
+    savingFolder = d;
 
-	setAutomaticSavingMode(automaticSaving);
+    setAutomaticSavingMode(automaticSaving);
 }
 
 void RenderingEncoder::saveFile(QString suffix, QString filename){
@@ -531,11 +531,11 @@ void RenderingEncoder::saveFile(QString suffix, QString filename){
     QFileInfo infoFileDestination(savingFolder, filename);
 
     // delete file if exists
-	if (infoFileDestination.exists()){
-		infoFileDestination.dir().remove(infoFileDestination.fileName());
+    if (infoFileDestination.exists()){
+        infoFileDestination.dir().remove(infoFileDestination.fileName());
     }
 
-	// move the temporaryFileName to newFileName
+    // move the temporaryFileName to newFileName
     if (!temporaryFolder.rename(temporaryFileName, infoFileDestination.fileName()) )
         qWarning() << infoFileDestination.absoluteFilePath() << QChar(124).toLatin1() << tr("Could not save file (file exists already?).");
     else {
@@ -552,12 +552,18 @@ void RenderingEncoder::saveFileAs(QString suffix, QString description){
                                                               description,
                                                               suffix,
                                                               suggestion);
-
     // if we got a filename, save the file:
     if (!newFileName.isEmpty()) {
 
-        saveFile(suffix, newFileName);
-
+        // delete file if exists
+        QFileInfo infoFileDestination(newFileName);
+        if (infoFileDestination.exists()){
+            infoFileDestination.dir().remove(infoFileDestination.fileName());
+        }
+        // move the temporaryFileName to newFileName
+        temporaryFolder.rename(temporaryFileName, newFileName);
+        emit status(tr("File %1 saved.").arg(newFileName), 2000);
+        qDebug() << newFileName << QChar(124).toLatin1() << tr("Recording saved.");
     }
 
 }
@@ -568,16 +574,16 @@ void RenderingEncoder::setBufferSize(unsigned long bytes){
 }
 
 unsigned long RenderingEncoder::getBufferSize() {
-    
+
     return bufferSize;
 }
 
 unsigned long RenderingEncoder::computeBufferSize(int percent) {
-    
+
     long double p = (double) CLAMP(percent, 0, 99) / 100.0;
     unsigned long megabytes = MIN_RECORDING_BUFFER_SIZE;
     megabytes += (unsigned long) ( p * (long double)(MAX_RECORDING_BUFFER_SIZE - MIN_RECORDING_BUFFER_SIZE));
-    
+
     return megabytes;
 }
 
@@ -585,6 +591,6 @@ int RenderingEncoder::computeBufferPercent(unsigned long bytes) {
 
     unsigned long b = bytes - MIN_RECORDING_BUFFER_SIZE;
     double p = (double) b / (double)(MAX_RECORDING_BUFFER_SIZE - MIN_RECORDING_BUFFER_SIZE);
-    
+
     return ( (int) qRound(p * 100.0) );
 }
