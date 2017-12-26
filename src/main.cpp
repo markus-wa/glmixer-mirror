6b565c98e0746752815a8912bf0c4a5bb20280c6
brunoherbelin <brunoherbelin@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2017-12-26 10:36:30 +0000

    Add Custom QApplication to handle OSX file openning events.
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@1521 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,7 @@
  *
  */
 
-#include <QtGui/QApplication>
-#include <QString>
-#include <QTextCodec>
-
+#include "main.moc"
 #include "common.h"
 #include "glmixer.h"
 #include "RenderingManager.h"
@@ -38,6 +35,63 @@
 #endif
 #endif
 
+#include <QTextCodec>
+
+
+GLMixerApp::GLMixerApp(int& argc, char** argv): QApplication(argc, argv), _filename(QString::null)
+{
+   // -1. sets global application name ; this is used application wide (e.g. QSettings)
+    setOrganizationName("bhbn");
+    setOrganizationDomain("bhbn.free.fr");
+    setApplicationName("GLMixer");
+
+#ifdef GLMIXER_VERSION
+    setApplicationVersion( QString("%1").arg(GLMIXER_VERSION, 2, 'f', 1 ) );
+#else
+    setApplicationVersion( "Beta" );
+#endif
+
+#ifndef Q_OS_WIN32
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("utf8"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
+#endif
+
+}
+
+void GLMixerApp::requestOpenFile() 
+{
+    // if a filename was given
+    if (!_filename.isNull()) {
+        // request to GLMixer to open the file (connected)
+        emit filenameToOpen(_filename);
+        // clear file name
+        _filename = QString();
+    }
+}
+
+void GLMixerApp::setFilenameToOpen(QString filename) 
+{
+    // update the file name only if not already set
+    if (_filename.isNull())
+        _filename = filename; 
+}
+  
+bool GLMixerApp::event(QEvent *event) 
+{
+    // The system requested us to open a file
+    if (event->type() == QEvent::FileOpen)  {
+        // Get the path of the file that we want to open
+        _filename = static_cast<QFileOpenEvent *> (event)->file();
+        // inform about file to open
+        emit filenameToOpen(_filename);
+    }
+    // The system requested us to do another thing, so we just follow the rules
+    else
+        return QApplication::event (event);
+
+    return true;
+}
+
 void testRegExp(QString pattern, Qt::CaseSensitivity cs, QRegExp::PatternSyntax syntax,  QString text)
 {
     QRegExp regex(pattern, cs, syntax);
@@ -51,25 +105,9 @@ int main(int argc, char **argv)
     int returnvalue = -1;
 
     //
-    // 1. Create the Qt application and treat arguments
+    // 0. Create the Qt application and treat arguments
     //
-    QApplication a(argc, argv);
-
-    // -1. sets global application name ; this is used application wide (e.g. QSettings)
-    a.setOrganizationName("bhbn");
-    a.setOrganizationDomain("bhbn.free.fr");
-    a.setApplicationName("GLMixer");
-
-#ifdef GLMIXER_VERSION
-    a.setApplicationVersion( QString("%1").arg(GLMIXER_VERSION, 2, 'f', 1 ) );
-#else
-    a.setApplicationVersion( "Beta" );
-#endif
-
-#ifndef Q_OS_WIN32
-    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("utf8"));
-    QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
-#endif
+    GLMixerApp a(argc, argv);
 
     // get the arguments into a list
     QStringList cmdline_args = a.arguments();
@@ -101,10 +139,9 @@ int main(int argc, char **argv)
         exit(returnvalue);
 
     // maybe argument is a filename ?
-    QString filename = QString::null;
     if ( cmdline_args.count() > 1 ) {
         if (QFileInfo(cmdline_args.at(1)).isFile())
-            filename = cmdline_args.at(1);
+            a.setFilenameToOpen( cmdline_args.at(1) );
         else
             qDebug("%s : invalid arguments (not a file name).", qPrintable(cmdline_args.at(1)));
     }
@@ -116,7 +153,7 @@ int main(int argc, char **argv)
     QPixmap pixmap(":/glmixer/images/glmixer_splash.png");
     QSplashScreen splash(pixmap);
 #ifdef GLMIXER_REVISION
-    splash.showMessage(QString("r%1         (%2)").arg(GLMIXER_REVISION).arg(COMPILE_YEAR));
+    splash.showMessage(QString("r%1 (%2)").arg(GLMIXER_REVISION).arg(COMPILE_YEAR));
 #endif
     splash.show();
     a.processEvents();
@@ -153,6 +190,7 @@ int main(int argc, char **argv)
     // 3. Start the application GUI
     //
     GLMixer::getInstance()->readSettings( a.applicationDirPath() );
+    QObject::connect(&a, SIGNAL(filenameToOpen(QString)), GLMixer::getInstance(), SLOT(switchToSessionFile(QString)));
     a.processEvents();
 
 #ifdef GLM_SHM
@@ -177,9 +215,8 @@ int main(int argc, char **argv)
     //
     // 4. load eventual session file provided in argument or restore last session
     //
-    if (filename.isNull())
-        filename = GLMixer::getInstance()->getRestorelastSessionFilename();
-    GLMixer::getInstance()->switchToSessionFile(filename);
+    a.setFilenameToOpen( GLMixer::getInstance()->getRestorelastSessionFilename() );
+    a.requestOpenFile();
 
     // start application loop
     returnvalue = a.exec();
