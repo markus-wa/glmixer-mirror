2556464e2fd6bb570db9df99f7c12dbd47b0c064
brunoherbelin <brunoherbelin@a4deb95c-d308-4ec4-8dc1-6419bbeeb61a>
2019-01-13 19:01:12 +0000

    BugFix: restore catalog with loading glm session
    git-svn-id: svn://svn.code.sf.net/p/glmixer/Source/trunk@1903 a4deb95c-d308-4ec4-8dc1-6419bbeeb61a

--- src/glmixer.cpp
+++ src/glmixer.cpp
@@ -586,7 +586,7 @@ GLMixer::GLMixer ( QWidget *parent): QMainWindow ( parent ),
     QObject::connect(actionCatalogLarge, SIGNAL(triggered()), RenderingManager::getRenderingWidget(), SLOT(setCatalogSizeLarge()));
 
     // Signals between GUI and rendering widget
-    QObject::connect(actionShow_Catalog, SIGNAL(triggered(bool)), RenderingManager::getRenderingWidget(), SLOT(setCatalogVisible(bool)));
+    QObject::connect(actionShow_Catalog, SIGNAL(toggled(bool)), RenderingManager::getRenderingWidget(), SLOT(setCatalogVisible(bool)));
     QObject::connect(actionWhite_background, SIGNAL(toggled(bool)), RenderingManager::getInstance(), SLOT(setClearToWhite(bool)));
     QObject::connect(sliderZoom, SIGNAL(valueChanged(int)), RenderingManager::getRenderingWidget(), SLOT(zoom(int)));
     QObject::connect(RenderingManager::getRenderingWidget(), SIGNAL(zoomPercentChanged(int)), sliderZoom, SLOT(setValue(int)));
